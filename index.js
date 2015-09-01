b90c5d0054ee95d541bebed861ac90a9d36d903c
Ben Drucker <bvdrucker@gmail.com>
2015-09-01 09:15:49 -0700

    Use browser-async-store as engine

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
 'use strict'
 
-var dezalgo = require('dezalgo')
-var store = require('./store')
+var createStore = require('browser-async-store')
+var storage = require('./store')
 
-exports.get = function get (key, callback) {
-  var value = store.getItem(key)
-  dezalgo(callback)(null, value != null ? JSON.parse(value) : null)
-}
-
-exports.put = function put (key, value, callback) {
-  store.setItem(key, JSON.stringify(value))
-  dezalgo(callback || noop)(null)
-}
-
-exports.remove = function remove (key, callback) {
-  store.removeItem(key)
-  dezalgo(callback || noop)(null)
-}
-
-exports.clear = function clear (callback) {
-  store.clear()
-  dezalgo(callback || noop)(null)
-}
-
-function noop () {}
+module.exports = createStore(storage)
