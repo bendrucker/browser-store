2b3a744b8c1d9d9c3f706c2635dcbd89643369d8
Ben Drucker <bvdrucker@gmail.com>
2015-10-01 15:13:49 -0700

    Replace node-localStorage with a sham that does not try to use localStorage

--- storage.js
+++ storage.js
@@ -2,7 +2,7 @@
 
 var hasLocal = require('has-local-storage')
 var hasSession = require('has-session-storage')
-var shim = require('localStorage')
+var Storage = require('storage-sham')
 var window = require('global/window')
 
-module.exports = hasLocal ? window.localStorage : hasSession ? window.sessionStorage : shim
+module.exports = hasLocal ? window.localStorage : hasSession ? window.sessionStorage : Storage()
